707877a7c355b3a306dc031682fc7ad79c55bdeb
SimbolKing <2736389388@qq.com>
2026-03-06 11:17:56 +1100

    create backend code for account system

---
+++ backend/web/views/user/account/login.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from web.models.user import UserProfile
+
+
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            username = request.data.get('username').strip()
+            password = request.data.get('password').strip()
+            if not username or not password:
+                return Response({
+                    'result': 'username and password can not be empty'
+                })
+            user = authenticate(username=username, password=password)
+            if user:
+                user_profile = UserProfile.objects.get(username=username)
+                refresh = RefreshToken.for_user(user)
+                response = Response({
+                    'result': 'success',
+                    'access': str(refresh.access_token),
+                    'user_id': user.id,
+                    'username': user.username,
+                    'photo': user_profile.photo.url,
+                    'profile': user_profile.profile,
+                })
+                response.set_cookie(
+                    key='refresh_token',
+                    value=str(refresh),
+                    httponly=True,
+                    samesite='Lax',
+                    secure=True,
+                    max_age=86400 * 7
+                )
+                return response
+            return Response({
+                'result': 'username or password is wrong'
+            })
+        except:
+            return Response({
+                'result': 'System error'
+            })
