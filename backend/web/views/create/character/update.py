0659de2b2a1b32e1f6131b5d507da3a1984af13a
SimbolKing <2736389388@qq.com>
2026-03-07 19:15:28 +1100

    create character backend code

---
+++ backend/web/views/create/character/update.py
@@ -0,0 +1,47 @@
+from django.utils.timezone import now
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from web.models.character import Character
+from web.views.utils.photo import remove_old_photo
+
+
+class UpdateCharacterView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        try:
+            character_id = request.data['character_id']
+            character = Character.objects.get(id=character_id, author__user=request.user)
+            name = request.data['name'].strip()
+            profile = request.data['profile'].strip()[:100000]
+            photo = request.FILES.get('photo', None)
+            background_image = request.FILES.get('background_image', None)
+
+            if not name:
+                return Response({
+                    'result': 'name can not be empty'
+                })
+            if not profile:
+                return Response({
+                    'result': 'character profile can not be empty'
+                })
+            if not photo:
+                remove_old_photo(character.photo)
+                character.photo = photo
+            if not background_image:
+                remove_old_photo(character.background_image)
+                character.background_image = background_image
+
+            character.name = name
+            character.profile = profile
+            character.update_time = now()
+            character.save()
+
+            return Response({
+                'result': 'success'
+            })
+        except:
+            Response({
+                'result': 'System error'
+            })
