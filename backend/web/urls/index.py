1bad96749f7274fdf3841a669ae44e4bddb1ed8c
SimbolKing <2736389388@qq.com>
2026-03-10 15:56:11 +1100

    add backend code for account system

--- backend/web/urls/index.py
+++ backend/web/urls/index.py
@@ -1,13 +1,8 @@
-from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import path, include
 
 from web.views.index import index
 
 urlpatterns = [
     path('', index),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/user/account/', include('web.urls.user.account')),
 ]
---
+++ backend/web/urls/user/account.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from web.views.index import index
+from web.views.user.account.login import LoginView
+from web.views.user.account.logout import LogoutView
+from web.views.user.account.refresh_token import RefreshTokenView
+from web.views.user.account.register import RegisterView
+
+urlpatterns = [
+    path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('refresh_token/', RefreshTokenView.as_view())
+]
