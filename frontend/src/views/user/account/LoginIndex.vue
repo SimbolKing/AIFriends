14a701cdf3d3263b8a95ad35a8b7bcfc8f2b6bdc
SimbolKing <2736389388@qq.com>
2026-03-10 15:04:46 +1100

    add frontend code for login/register system

--- frontend/src/views/user/account/LoginIndex.vue
+++ frontend/src/views/user/account/LoginIndex.vue
@@ -1,5 +1,16 @@
 <template>
-login
+    <div class="flex justify-center mt-20">
+        <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            <label class="label">Username</label>
+            <input type="text" class="input" placeholder="Username" />
+
+            <label class="label">Password</label>
+            <input type="password" class="input" placeholder="Password" />
+
+            <button class="btn btn-neutral mt-4">Login</button>
+            <RouterLink :to="{name: 'user-account-register-index'}" class="btn btn-ghost">Register</RouterLink>
+        </fieldset>
+    </div>
 </template>
 
 <script setup>
--- frontend/src/views/user/account/RegisterIndex.vue
+++ frontend/src/views/user/account/RegisterIndex.vue
@@ -1,5 +1,19 @@
 <template>
-register
+    <div class="flex justify-center mt-20">
+        <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            <label class="label">Username</label>
+            <input type="text" class="input" placeholder="Username" />
+
+            <label class="label">Password</label>
+            <input type="password" class="input" placeholder="Password" />
+
+            <label class="label">Confirm your password</label>
+            <input type="password" class="input" placeholder="Confirm your password" />
+
+            <button class="btn btn-neutral mt-4">Register</button>
+            <RouterLink :to="{name: 'user-account-login-index'}" class="btn btn-ghost">Login</RouterLink>
+        </fieldset>
+    </div>
 </template>
 
 <script setup>
