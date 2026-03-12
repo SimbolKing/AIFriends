09e2f63eba5b8a658eafe5c1133f12cb31380337
SimbolKing <2736389388@qq.com>
2026-03-12 16:10:09 +1100

    add frontend code for update user profile

---
+++ frontend/src/views/user/profile/components/Username.vue
@@ -0,0 +1,25 @@
+<template>
+    <fieldset class="fieldset">
+        <label class="label text-base">Username</label>
+        <input v-model="myUsername" type="text" class="input w-108">
+    </fieldset>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+
+const props = defineProps(['username'])
+const myUsername = ref(props.username)
+
+watch(() => props.username, newVal => {
+    myUsername.value = newVal
+})
+
+defineExpose({
+    myUsername,
+})
+</script>
+
+<style scoped>
+
+</style>
